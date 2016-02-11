b0c701f29ad51600423c8adc028845fbd1be6dfb
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-02-11 19:25:36 +0900

    fizz buzz作り始める

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
 var assert = require("power-assert");
 
-var test = "hoge";
-it ("assert", function() {
-    // assert(test === 'hoge');  // こっち成功
-    assert(test === 'fuga');  // こっち失敗
+var mainLogic = {
+    run: function() {
+        return 1;
+    }
+};
+
+describe('fizz buzz test!!', function() {
+    describe('最初の場合', function() {
+        it ("1が出る事", function() {
+            assert(mainLogic.run() === 1);
+        });
+    });
 });
